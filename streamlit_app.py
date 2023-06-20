9e59f3026fc83079e03927c49a9256611a7b0406
ezra102216 <137042298+ezra102216@users.noreply.github.com>
2023-06-20 18:26:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,9 +50,9 @@ except URLError as e:
 
 
 
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+#my_data_row = my_cur.fetchone()
+#streamlit.text("The fruit load list contains:")
+#streamlit.text(my_data_row)
 
 
 
