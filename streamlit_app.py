93e99f4fea823af6fdb785f3135a65e9340046b1
ezra102216 <137042298+ezra102216@users.noreply.github.com>
2023-06-21 08:11:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,8 +66,8 @@ def get_fruit_load_list():
             
 if streamlit.button('Get fruit Load Please'):
    
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
+my_data_rows = get_fruit_load_list()
+streamlit.dataframe(my_data_rows)
 
 
 
